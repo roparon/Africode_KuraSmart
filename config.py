3930742d70bab3906e211e1eb46c2bbb39466cb4
Aron Rop <aaronrop40@gmail.com>
2025-06-11 20:37:06 +0300

    successfully set up authentication and tested the protected route,

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,3 +8,8 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///kura.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT Configurations ##
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")  # Should be in .env
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)  # short-lived access token
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)    # long-lived refresh token
