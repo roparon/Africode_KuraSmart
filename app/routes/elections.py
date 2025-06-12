6b1f4effa767fb4da1479fdeec085776302dcdda
Aron Rop <aaronrop40@gmail.com>
2025-06-12 10:17:15 +0300

    created routes for election, geeting by id, listing and deleting

--- app/routes/elections.py
+++ app/routes/elections.py
@@ -1,4 +1,3 @@
-# app/routes/elections.py
 from flask import Blueprint, request, jsonify
 from app.extensions import db
 from app.models import Election, User
@@ -27,3 +26,55 @@ def create_election():
     db.session.commit()
 
     return jsonify({'message': 'Election created', 'election_id': election.id}), 201
+
+
+#get list of election
+@elections_bp.route('/elections', methods=['GET'])
+def get_elections():
+    elections = Election.query.all()
+
+    result = []
+    for election in elections:
+        result.append({
+            'id': election.id,
+            'title': election.title,
+            'description': election.description,
+            'start_date': election.start_date,
+            'end_date': election.end_date,
+            'is_active': election.is_active
+        })
+
+    return jsonify(result), 200
+
+#get by id
+@elections_bp.route('/elections/<int:election_id>', methods=['GET'])
+def get_election(election_id):
+    election = Election.query.get_or_404(election_id)
+
+    return jsonify({
+        'id': election.id,
+        'title': election.title,
+        'description': election.description,
+        'start_date': election.start_date,
+        'end_date': election.end_date,
+        'is_active': election.is_active
+    }), 200
+
+
+#delete/deactivate electio
+@elections_bp.route('/elections/<int:election_id>/deactivate', methods=['PATCH'])
+@jwt_required()
+def deactivate_election(election_id):
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user or user.role != 'admin' or not user.is_verified:
+        return jsonify({'error': 'Unauthorized'}), 403
+
+    election = Election.query.get_or_404(election_id)
+    election.is_active = False
+    db.session.commit()
+
+    return jsonify({'message': 'Election deactivated'}), 200
+
+
