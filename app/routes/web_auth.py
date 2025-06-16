b810c9ed4838cabff15ac3ac94b83b8525114589
Aron Rop <aaronrop40@gmail.com>
2025-06-16 10:11:11 +0300

    created a registration html file

--- app/routes/web_auth.py
+++ app/routes/web_auth.py
@@ -1,9 +1,39 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
-from app.forms.forms import LoginForm
+from app.forms.forms import LoginForm, RegistrationForm
 from app.models import User
+from app.extensions import db
 
 web_auth_bp = Blueprint('web_auth', __name__)
+
+
+@web_auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if User.query.filter_by(email=form.email.data).first():
+            flash('Email already registered.', 'danger')
+            return render_template('register.html', form=form)
+
+        user = User(
+            full_name=form.full_name.data,
+            email=form.email.data,
+            role='voter'  # or dynamic role
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('web_auth.login'))
+
+    return render_template('register.html', form=form)
+
+
+
+
+
+
+
 
 @web_auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
