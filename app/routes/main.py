231c661abefc4e99cd87ed2d2238bfc33330595d
Aron Rop <aaronrop40@gmail.com>
2025-06-16 13:05:47 +0300

    created a registration html file

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, url_for
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('main.index'))
+
