5693444d9343b3ce6866fcca564ec9b0515225c7
Aron Rop <aaronrop40@gmail.com>
2025-06-16 15:15:35 +0300

    created a registration html file

--- app/routes/web_auth.py
+++ app/routes/web_auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from app.forms.forms import LoginForm, RegistrationForm
 from app.models import User
 from app.extensions import db
@@ -25,10 +25,11 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        flash('Registration successful! Please log in.', 'success')
+        flash(f'{user.full_name}, registered successful! Please log in.', 'success')
         return redirect(url_for('web_auth.login'))
 
     return render_template('register.html', form=form)
+
 
 
 #User loginoroute
@@ -40,15 +41,13 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('main.index'))
-        else:
-            flash('Invalid email or password.', 'danger')
+            flash(f'{user.full_name} logged in successfully.', 'success')
+
+            if user.role == 'admin':
+                return redirect(url_for('admin.dashboard'))
+            else:
+                return redirect(url_for('voter.dashboard'))
+
+        flash('Invalid email or password.', 'danger')
     return render_template('login.html', form=form)
 
-@web_auth_bp.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logged out successfully.', 'info')
-    return redirect(url_for('web_auth.login'))
