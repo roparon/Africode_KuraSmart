98ad7f6fb9866572f48c3298e16c7a1c9fb2d160
Aron Rop <aaronrop40@gmail.com>
2025-06-17 10:06:54 +0300

    Corrected Model Relationship

---
+++ app/utils/decorators.py
@@ -0,0 +1,13 @@
+from flask import abort
+from flask_login import current_user
+from functools import wraps
+
+def role_required(*roles):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(*args, **kwargs):
+            if not current_user.is_authenticated or current_user.role not in roles:
+                abort(403)
+            return view_func(*args, **kwargs)
+        return wrapper
+    return decorator
