92b44c7264852c6a1c46432424529f553038ea52
Aron Rop <aaronrop40@gmail.com>
2025-06-19 10:27:50 +0300

    create an election in your Flask web app (like an admin panel for election management),

--- app/super_admin/create.py
+++ app/super_admin/create.py
@@ -21,3 +21,14 @@ with app.app_context():
         print("Super admin created successfully.")
     else:
         print("⚠️ Super admin already exists.")
+
+
+        super_admin = User(
+    full_name="Aron Rop",
+    username="roparon",
+    email="aaronrop40@gmail.com",
+    password_hash=generate_password_hash("0987654321"),
+    role=UserRole.super_admin,
+    is_verified=True,
+    is_superadmin=True
+)
