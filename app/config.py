f24dd90aeaead7c941c0c1bd5904c0826ba01663
Aron Rop <aaronrop40@gmail.com>
2025-06-24 21:20:13 +0300

    created forgot and reset password functionlity

--- app/config.py
+++ app/config.py
@@ -12,21 +12,13 @@ class Config:
     SESSION_COOKIE_SECURE = False
     REMEMBER_COOKIE_DURATION = 3600
 
-    # Mail Configuration
+    # Mail Configuration (only once)
     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
     MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
     MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
     MAIL_USERNAME = os.getenv("MAIL_USERNAME", "aaronrop40@gmail.com")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
 
     TIMEZONE = 'Africa/Nairobi'
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
-
- 
