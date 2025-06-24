bb150f1e9546797436e31463fedcceec5671b2fe
Aron Rop <aaronrop40@gmail.com>
2025-06-24 16:46:10 +0300

    created forgot and reset password functionlity

--- config.py
+++ config.py
@@ -18,12 +18,12 @@ class Config:
     # JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     # JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
-    
+
 # Flask‑Mail configuration
 MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))
 MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
 MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
 MAIL_USERNAME = os.getenv("MAIL_USERNAME", "aaronrop40@gmail.com")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "rh2030oz")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
 MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
