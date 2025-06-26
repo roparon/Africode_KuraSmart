c0d501bdf516a2cd0e5aacfb52d8e9507ce053ce
Aron Rop <aaronrop40@gmail.com>
2025-06-26 16:15:49 +0300

    crUpdated Dashboard Template (Glass UI Applied)

--- app/routes/web_auth.py
+++ app/routes/web_auth.py
@@ -278,6 +278,37 @@ def bulk_delete_users():
     db.session.commit()
     return jsonify({'message': f'{len(users)} user(s) deleted'}), 200
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # -------------------------
 # Export Users to CSV
 # -------------------------
