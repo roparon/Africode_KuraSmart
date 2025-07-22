967c61ff6c30394ed5fd8c938228c838c5ac8551
Aron Rop <aaronrop40@gmail.com>
2025-07-22 19:49:32 +0300

    Updated my home route to logout the user when he/she visits homepage

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,flash, redirect, url_for
+from flask_login import current_user, logout_user
+
 
 main_bp = Blueprint('main', __name__)
 
+
 @main_bp.route('/')
 def index():
-    return render_template ('index.html')
+    if current_user.is_authenticated:
+        name = current_user.full_name
+        logout_user()
+        flash(f'{name}, you have been logged out automatically.', 'info')
+        return redirect(url_for('main.index'))
+    return render_template('index.html')
 
