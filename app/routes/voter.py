58d900ae56b13df2f74df48773053c2a2b5037bf
Aron Rop <aaronrop40@gmail.com>
2025-07-27 21:43:47 +0300

    Modified my election_details for better user text styling

--- app/routes/voter.py
+++ app/routes/voter.py
@@ -173,7 +173,6 @@ def cast_vote(election_id):
             flash('No candidate or position selected.', 'warning')
             return redirect(url_for('voter.view_election', election_id=election_id))
 
-        # 🧾 Check for duplicate vote
         existing_vote = Vote.query.filter_by(
             voter_id=current_user.id,
             election_id=election_id,
